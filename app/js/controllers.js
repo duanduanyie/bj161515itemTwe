ecf610622aee6e118f7c39ea216eff76d9a25dd0
lanou3g <472253656@lanou3g.com>
2017-05-09 22:04:23 +0800

    段哥又来了

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,6 +8,42 @@ controllerModule.controller("navC", function () {
 controllerModule.controller("businessC", function ($scope) {
     $scope.arr=[
         {
+            imgSrc:"../images/navImg/business-1.png",
+            name:"李杰",
+            number:"13698200146",
+            address:"山西省五台县"
+        },{
+            imgSrc:"../images/navImg/business-1.png",
+            name:"李杰",
+            number:"13698200146",
+            address:"山西省五台县"
+        },{
+            imgSrc:"../images/navImg/business-1.png",
+            name:"李杰",
+            number:"13698200146",
+            address:"山西省五台县"
+        },{
+            imgSrc:"../images/navImg/business-1.png",
+            name:"李杰",
+            number:"13698200146",
+            address:"山西省五台县"
+        },{
+            imgSrc:"../images/navImg/business-1.png",
+            name:"李杰",
+            number:"13698200146",
+            address:"山西省五台县"
+        },{
+            imgSrc:"../images/navImg/business-1.png",
+            name:"李杰",
+            number:"13698200146",
+            address:"山西省五台县"
+        },{
+            imgSrc:"../images/navImg/business-1.png",
+            name:"李杰",
+            number:"13698200146",
+            address:"山西省五台县"
+        },{
+            imgSrc:"../images/navImg/business-1.png",
             name:"李杰",
             number:"13698200146",
             address:"山西省五台县"
